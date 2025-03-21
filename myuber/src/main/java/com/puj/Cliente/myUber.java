4073e9909adc11d62f2892bbe50eb64425d59fc3
Pablo Enrique Quintero Callamand <140966677+QuinteroEP@users.noreply.github.com>
2025-03-21 09:45:10 -0500

    v0.2.3

--- myuber/src/main/java/com/puj/Cliente/myUber.java
+++ myuber/src/main/java/com/puj/Cliente/myUber.java
@@ -3,5 +3,7 @@ package com.puj.Cliente;
 public class myUber {
     public static void main(String[] args){
         System.out.println("Esperando servidor");
+
+        System.out.println("\t||Bienvenido a MyUber||");
     }
 }
--- myuber/src/main/java/com/puj/Servidor/servidor.java
+++ myuber/src/main/java/com/puj/Servidor/servidor.java
@@ -3,5 +3,8 @@ package com.puj.Servidor;
 public class servidor {
     public static void main(String[] args){
         System.out.println("Inicializando servidor");
+
+        System.out.println("Servidor iniciado en el puerto");
+        System.out.println("Esperando conexiones de clientes");
     }
 }
