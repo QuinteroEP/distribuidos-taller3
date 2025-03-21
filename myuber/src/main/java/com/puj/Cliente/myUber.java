a0f31fdcf637e09135e742c8cb5aa41a32da48d3
Pablo Enrique Quintero Callamand <pabloenriquequintero@gmail.com>
2025-03-21 13:42:31 -0500

    v0.2.4
    codigo para conexion

--- myuber/src/main/java/com/puj/Cliente/myUber.java
+++ myuber/src/main/java/com/puj/Cliente/myUber.java
@@ -1,9 +1,20 @@
 package com.puj.Cliente;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class myUber {
     public static void main(String[] args){
-        System.out.println("Esperando servidor");
+        String server = "192.168.10.6"; //Cambiar segund la IP del servidor
+        int port = 1080;
+    
+        System.out.println("Conectando con el servidor");
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(server, port)
+                .build();
 
         System.out.println("\t||Bienvenido a MyUber||");
+
+        channel.shutdown();
     }
 }
