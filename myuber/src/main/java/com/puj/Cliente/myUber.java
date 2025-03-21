f523254bbc5e6133cf0143c34eeb02c09b0efa2d
Pablo Enrique Quintero Callamand <140966677+QuinteroEP@users.noreply.github.com>
2025-03-21 09:33:55 -0500

    v0.2.2
    proto corregido

--- myuber/src/main/java/com/puj/Cliente/myUber.java
+++ myuber/src/main/java/com/puj/Cliente/myUber.java
@@ -1,5 +1,7 @@
 package com.puj.Cliente;
 
 public class myUber {
-    
+    public static void main(String[] args){
+        System.out.println("Esperando servidor");
+    }
 }
---
+++ myuber/src/main/java/com/puj/Servidor/servidor.java
@@ -0,0 +1,7 @@
+package com.puj.Servidor;
+
+public class servidor {
+    public static void main(String[] args){
+        System.out.println("Inicializando servidor");
+    }
+}
